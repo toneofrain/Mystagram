579c59e3790233689ee08f29e0a646fb49362612
toneofrain <toneofrain@gmail.com>
2023-04-06 18:59:04 +0900

    chore: initialize project (#01)

---
+++ src/main/java/com/saintho/mystagram/MystagramApplication.java
@@ -0,0 +1,13 @@
+package com.saintho.mystagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystagramApplication.class, args);
+	}
+
+}
